569770ca8ba3554538fbad72b343a2fdcad12f38
yash@8123 <116726926+yash08123@users.noreply.github.com>
2025-02-02 16:48:30 +0530

    Refactor redirect logic in login and signup views to use dynamic username

--- users/views.py
+++ users/views.py
@@ -16,11 +16,11 @@ def login_page(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'Login successful!')
-            return redirect('/chat/kishan/')
+            return redirect('/chat/'+username) #Redirect to chat page
         else:
             messages.error(request, 'Invalid email or password. Please try again.')
     if request.user.is_authenticated:
-        return redirect('/chat/kishan/')
+        return redirect(f'/chat/{request.user.username}/') #Redirect to chat page if user is already logged in
     return render(request,'login.html')
 
 
@@ -57,6 +57,6 @@ def signup_view(request):
         messages.success(request, 'Signup successful! You can now log in.')
         return redirect('login')
     if request.user.is_authenticated:
-        return redirect('/chat/kishan/') #todo: pass usename
+        return redirect(f'/chat/{request.user.username}/') #Redirect to chat page if user is already logged in
     return render(request, 'signup.html')
 
